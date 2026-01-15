7b53ef2cdeba612add41ed9eac0d26f630112df9
tristan_retali <tristan.retali@gmail.com>
2026-01-15 23:22:38 +0100

    Ajout du pydantic qui correspond aux labels nécessaire

--- api/routes.py
+++ api/routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
+from schemas import PredictPayload
 
 router = APIRouter(prefix="/api")
 
 
-@router.get("/predict")
-def predict():
-    return {"status": "API is working"}
+@router.post("/predict")
+def predict(payload: PredictPayload):
+    return payload.model_dump(by_alias=True)
---
+++ api/schemas.py
@@ -0,0 +1,31 @@
+from enum import Enum
+from pydantic import BaseModel, Field
+
+
+class Transmission(str, Enum):
+    Manual = "Manual"
+    Automatic = "Automatic"
+
+
+class FuelType(str, Enum):
+    Electric = "Electric"
+    Diesel = "Diesel"
+    Petrol = "Petrol"
+
+
+class Condition(str, Enum):
+    New = "New"
+    Like_New = "Like New"
+    Used = "Used"
+
+
+# Les '...' servent à indiquer que le champ est obligatoire
+# On utilise 'alias' pour correspondre aux noms des colonnes du DataFrame et donc celles
+# avec lesquels le modèle a été entraîné
+class PredictPayload(BaseModel):
+    year: int = Field(..., alias="Year")
+    engine_size: float = Field(..., alias="Engine Size")
+    fuel_type: FuelType = Field(..., alias="Fuel Type")
+    transmission: Transmission = Field(..., alias="Transmission")
+    mileage: int = Field(..., alias="Mileage")
+    condition: Condition = Field(..., alias="Condition")
