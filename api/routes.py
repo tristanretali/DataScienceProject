0d10b77305b07ac9b2d13f7ecc190aab332d0383
tristan_retali <tristan.retali@gmail.com>
2026-01-15 23:41:17 +0100

    Ajout de la version dans la route de génération de la prédiction

--- api/routes.py
+++ api/routes.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter
 from schemas import PredictPayload
 from config import MODELS_DIR
 
 router = APIRouter(prefix="/api")
 
 
-@router.post("/predict")
-def predict(payload: PredictPayload):
+@router.post("/{version}/predict")
+def predict(version: str, payload: PredictPayload):
+    if version not in ["v1", "v2"]:
+        return {"error": f"La version {version} n'est pas existante. Utilisez v1 ou v2"}
     return payload.model_dump(by_alias=True)
